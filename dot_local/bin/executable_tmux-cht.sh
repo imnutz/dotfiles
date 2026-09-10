e3e0c8accbdd0513dfdf05d62af6b673101646d6
Son Do <son.do@treasure-data.com>
2026-09-10 07:06:46 +0700

    update

--- dot_local/bin/executable_tmux-cht.sh
+++ dot_local/bin/executable_tmux-cht.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+selected=$(cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf)
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+if grep -qxsF -- "$selected" ~/.tmux-cht-languages; then
+    tmux new-window \
+        -e "TMUX_CHT_SELECTED=$selected" \
+        -e "TMUX_CHT_QUERY=$query" \
+        'bash -c '\''query=${TMUX_CHT_QUERY// /+}; url="https://cht.sh/${TMUX_CHT_SELECTED}/${query}/"; printf "curl %s\\n" "$url"; curl "$url"; while :; do sleep 1; done'\'''
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux new-window \
+        -e "TMUX_CHT_SELECTED=$selected" \
+        -e "TMUX_CHT_QUERY=$query" \
+        'bash -c '\''query=${TMUX_CHT_QUERY// /+}; curl -s -- "https://cht.sh/${TMUX_CHT_SELECTED}~${query}" | less'\'''
 fi
